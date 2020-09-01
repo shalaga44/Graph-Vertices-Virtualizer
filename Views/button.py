b61b1047c41fec75d8e800ccfbd40b7a02a74e6b
Mohamed Moawia <mohamedmoawia4@gmail.com>
2020-09-01 23:24:33 +0200

    Add auto set for width & height

--- Views/button.py
+++ Views/button.py
@@ -1,4 +1,4 @@
-from typing import Callable
+from typing import Callable, Tuple
 
 import pygame
 from pygame import display, Surface
@@ -10,24 +10,27 @@ from Mangers.font_manager import FontManager
 
 
 class Button:
-    def __init__(self, _text: str, _pos: Pos, _width: int = 100, _height: int = 100):
+    def __init__(self, _text: str, _pos: Pos, _width: int = None, _height: int = None):
         self._width: int = _width
         self._height: int = _height
         self._text = str(_text)
         self._pos: Pos = _pos
-        self.colorText = Colors.MainColors.surfaceColor
-        self.colorBackground = Colors.ButtonColors.default
-        self.colorBackgroundOnClick = Colors.ButtonColors.defaultOnClick
-        self._rect: RectType = self._createRect(_pos.x, _pos.y, _width, _height)
+        textSize = self._getTextSize()
+        self._setupWidthAndHeight(textSize)
+        self.colorText = Colors.ButtonColors.defaultText
+        # self.colorTextOnClick = Colors.ButtonColors.defaultTextOnClick
+        self.colorBackground = Colors.ButtonColors.defaultBackground
+        self.colorBackgroundOnClick = Colors.ButtonColors.defaultBackgroundOnClick
         self._textImage: Surface = self._creatText(_text)
+        self._rect: RectType = self._createRect()
         self.action: Callable = lambda: print(f"Button {self._text}::onClick()")
         self.isClicked = False
         self.clickDurationAnim = 0
         self.clickDurationAnimLimit = 5
 
-    @staticmethod
-    def _createRect(left, top, _width, height) -> RectType:
-        rect = pygame.rect.Rect(left, top, _width, height)
+    def _createRect(self) -> RectType:
+        rect = pygame.rect.Rect(self._pos.x, self._pos.y, self._width,
+                                self._height)
         return rect
 
     def _drawRect(self, screen: display):
@@ -48,9 +51,9 @@ class Button:
         font = FontManager().font
         textImage = font.render(self._text, True, self.colorText)
         wText, hText = font.size(self._text)
-        self.wTextHalf, self.hTextHalf = wText // 2, hText // 2
-        self.textPos = (self._pos.x + (self._width // 2)) - self.wTextHalf, (
-                self._pos.y + (self._height // 2)) - self.hTextHalf
+        wTextHalf, hTextHalf = wText // 2, hText // 2
+        self.textPos = (self._pos.x + (self._width // 2)) - wTextHalf, (
+                self._pos.y + (self._height // 2)) - hTextHalf
         self._textImage = textImage
         return textImage
 
@@ -60,3 +63,15 @@ class Button:
 
     def setOnClick(self, func: Callable):
         self.action = func
+
+    def _setupWidthAndHeight(self, textSize: Tuple[int, int]):
+        textWidth, textHeight = textSize
+        if self._width is None:
+            self._width = textWidth + 20
+        if self._height is None:
+            self._height = textHeight + 3
+
+    def _getTextSize(self) -> Tuple[int, int]:
+        font = FontManager().font
+        textWidth, textHeight = font.size(self._text)
+        return textWidth, textWidth
