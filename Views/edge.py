f6aabed6eaef4ee121351b5adf9939b738a0fb84
Mohamed Moawia <mohamedmoawia4@gmail.com>
2020-09-02 23:38:34 +0200

    Rename parameters

--- Views/edge.py
+++ Views/edge.py
@@ -4,10 +4,10 @@ from Views.vertex import Vertex
 
 
 class Edge:
-    def __init__(self, fromVertex: Vertex, toVertex: Vertex):
-        self.start: Vertex = fromVertex
-        self.end: Vertex = toVertex
-        self._str = f"{fromVertex}->{toVertex}"
+    def __init__(self, startEdge: Vertex, endEdge: Vertex):
+        self.start: Vertex = startEdge
+        self.end: Vertex = endEdge
+        self._str = f"{startEdge}->{endEdge}"
 
     def __str__(self):
         return self._str
